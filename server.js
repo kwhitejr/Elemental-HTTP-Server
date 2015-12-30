b659c268f9fa53864ad50125fb26c02865d80850
kwhitejr <kwhitejr.edu@gmail.com>
2015-12-29 20:46:10 -1000

    POST working. Need to update index.html

--- server.js
+++ server.js
@@ -60,14 +60,15 @@ var server = http.createServer(function requestHandler (req, res) {
       // Create new .html based upon posted elementName
 
       case 'POST':
-        console.log('POST request triggered.');
-
-        // Does JSON.stringify do the same thing as qs.parse?
         var stringifiedData = JSON.stringify(data);
+        console.log('POST request triggered.');
+        console.log(data);
+        console.log(stringifiedData);
+        // Does JSON.stringify do the same thing as qs.parse?
 
         if (req.url === '/elements') {
           console.log('POST has made contact...');
-          fs.writeFile('public/' + data.elementName + '.html', pageBuild(data), function (err) {
+          fs.writeFile('./public/' + data.elementName + '.html', pageBuild(data), function (err) {
             console.log('Time to write the file...');
 
             // Error condition
@@ -81,18 +82,19 @@ var server = http.createServer(function requestHandler (req, res) {
             res.writeHead(200, {
               'Content-Type': 'application/json'
             });
-            res.end({
+            res.end(JSON.stringify({
               'success': true
-            });
+            }));
             console.log('Data has been stored');
 
             // If the Post Req elementName is not in the directory of elements, add it to the directory
             if(elements.indexOf(data.elementName) === -1) {
               elements.push(data.elementName);
               console.log(elements);
-              updateIndex(elements); // a function to update 'index.html'
+              updateIndex(data.elementName); // a function to update 'index.html'
             }
           });
+          console.log('New elements array: ', elements);
         }
       break;
 
@@ -102,7 +104,7 @@ var server = http.createServer(function requestHandler (req, res) {
         console.log('GET request triggered.');
         if (req.url === '/') {
           console.log('index.html loading...');
-          fs.readFile('public/index.html', function (err, data) {
+          fs.readFile('public/index.html', 'utf8', function (err, data) {
 
             // Error condition
             if (err) {
@@ -115,26 +117,27 @@ var server = http.createServer(function requestHandler (req, res) {
           });
         // If specific request, get that page. If no page, return 404 error.
         } else {
-          fs.readFile('public' + req.url, function (err, data) {
+          fs.readFile('public' + req.url, 'utf8', function (err, data) {
 
             // Error condition
             if (err) {
-              console.error(err);
-              fs.readFile('public/404.html', function (err, rawbuffer) {
-
-                // Error condition to the error condition...
-                if (err) {
-                  res.statusCode = 500;
-                  res.statusMessage = "Could not find 404...";
-                  return res.end();
-                }
-                res.end(rawbuffer);
-              });
+              console.log('Requested page does not exist.');
               res.statusCode = 404;
               res.statusMessage = "Could not GET " + req.url;
-              return res.end();
+              return fs.readFile('./public/404.html', 'utf8', function (err, rawbuffer) {
+                console.log('Why you no read?');
+                console.log(rawbuffer);
+                // // Error condition to the error condition...
+                // if (err) {
+                //   res.statusCode = 500;
+                //   res.statusMessage = "Could not find 404...";
+                //   return res.end();
+                // }
+                return res.end(rawbuffer);
+              });
+              // return res.end(data);
             }
-            // Return the data, i.e. the requested url.
+            // (else) Return the data, i.e. the requested url.
             return res.end(data);
           });
         }
@@ -153,6 +156,7 @@ server.listen(8080, function () {
 
 // A function to build a new page based upon POST data.
 function pageBuild(data) {
+  // if (data.elementName) {}
   return '<html lang="en">' +
       '<head>' +
         '<meta charset="UTF-8">' +
@@ -171,10 +175,17 @@ function pageBuild(data) {
 
 // A function to update index.html to reflect newly added element page.
 // Question: how to talk specifically to index.html? '.load()'? '.get()'?
-function updateIndex(data) {
-  console.log('hello, new element friends: ', elements);
-  var $newElement = $('<li>' +
-    '<a href="/' + data.elementName + '.html>' + data.elementName + '</a>' +
-    '</li>');
-  $('ol').append($newElement);
+function updateIndex(element) {
+  // fs.readFile gets a string
+  var li = "<li><a href='/" +element+ ".html'>" +element+ "</a></li>";
+  var index = fs.readFile('index.html', 'utf8', function (err, data) {});
+  fs.writeFile('index.html', li, index.lastIndexOf('</li>')+5, 'utf8', function (err) {
+    if (err) {
+      res.statusCode = 500;
+      res.statusMessage = "Could not POST...";
+      return res.end();
+    }
+  });
 }
+
+// read in the index file, make sure it doesn't already have the element, write back the index file with the new element added
